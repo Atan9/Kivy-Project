23749cbf9a5ab7aa9b66cf4d3ae5ee1bbfd9816e
atan9 <andyctan91@gmail.com>
2021-09-19 21:40:51 -0500

    fixed the naming convetion and added .kv files to handle the layout of the app. fixed some function logic, added check text function

--- main.py
+++ main.py
@@ -1,81 +1,129 @@
 from kivy.app import App
-import random
-from kivy.uix.label import Label
-from kivy.uix.image import Image
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-red = [1, 0, 0, 1]
-green = [0, 1, 0, 1]
-blue = [0, 0, 1, 1]
-purple = [1, 0, 1, 1]
+# Set the app size
+Window.size = (500,700)
 
-class MainApp(App):
-    def build(self):
-        self.operators = ["/", "*", "+", "-"]
-        self.last_was_operator = None
-        self.last_button = None
-        main_layout = BoxLayout(orientation='vertical')
-        self.solution = TextInput(multiline = False, readonly = True, halign = "right", font_size = 55)
+# Designate Our .kv design file 
+Builder.load_file('calculator.kv')
+
+class MyLayout(Screen):
+    def clear(self):
+        self.ids.calc_input.text = '0'
+
+    # Create a button pressing function
+    def button_press(self, button):
+        # create a variable that contains whatever was in the text box already
+        prior = self.ids.calc_input.text
         
-        main_layout.add_widget(self.solution)
-
-        buttons = [
-            ["7", "8", "9", "/"],
-            ["4", "5", "6", "*"],
-            ["1", "2", "3", "-"],
-            [".", "0", "C", "+"],
-        ]
-
-        for row in buttons:
-            h_layout = BoxLayout()
-            for label in row:
-                button = Button(
-                    text=label,
-                    pos_hint={"center_x": .5, "center_y": .5},
-                )
-                button.bind(on_press=self.on_button_press)
-                h_layout.add_widget(button)
-            main_layout.add_widget(h_layout)
-
-
-        equals_button = Button(
-            text="=", pos_hint={"center_x":.5, "center_y": .5}
-        )
-        equals_button.bind(on_press=self.on_solution)
-        main_layout.add_widget(equals_button)
-
-
-
-        return main_layout
-
-    def on_button_press(self, instance):
-        current = self.solution.text
-        button_text = instance.text
-
-        if button_text == "C":
-            #clear the solution widget
-            self.solution.text = ""
+        # determine if 0 is sitting there
+        if prior == "0":
+                self.ids.calc_input.text = ''
+                self.ids.calc_input.text = f'{button}'
+        else: 
+            self.ids.calc_input.text = f'{prior}{button}'
+    
+    # remove last character in text box
+    def remove(self):
+        prior = self.ids.calc_input.text
+        # Remove The last item in the textbox
+        prior = prior[:-1]
+        # Output back to the textbox
+        self.ids.calc_input.text = prior
+    
+    # Create function to make text box positive or negative
+    def pos_neg(self):
+        prior = self.ids.calc_input.text
+        # Test to see if there's a - sign already
+        if "-" in prior:
+             self.ids.calc_input.text = f'{prior.replace("-", "")}'
         else:
-            if current and (self.last_was_operator and button_text in self.operators):
-                #dont add two operator right after each other
-                return
-            elif current == "" and button_text in self.operators:
-                # first character cannot be an operator
-                return
-            else:
-                new_text = current + button_text
-                self.solution.text = new_text
-        self.last_button = button_text
-        self.last_was_operator = self.last_button in self.operators
+            self.ids.calc_input.text = f'-{prior}'
 
-    def on_solution(self, instance):
-        text = self.solution.text
-        if text:
-            solution = str(eval(self.solution.text))
-            self.solution.text = solution
+    # Create decimal function
+    def dot(self):
+        prior = self.ids.calc_input.text
+        
+        if "." in prior:
+            pass
+        else:
+            # Add a decimal to the end of the text
+            prior = f'{prior}.'
+            # Output back to the text box
+            self.ids.calc_input.text = prior
+
+    # create addition function
+    def math_sign(self, sign):
+        curr = self.ids.calc_input.text
+        operator = ["*", "/", "-", "+"]
+        last_operator = curr[-1]
+        # check to make sure that operator sign wont be next to each other
+        if last_operator in operator:
+            temp_list = list(curr)
+            temp_list[-1] = sign
+            curr = ''.join(temp_list)
+        else:
+            curr = curr + sign
+        
+        self.ids.calc_input.text = curr
+
+
+    def equals(self):
+        prior = self.check_text(self.ids.calc_input.text)
+
+        if prior == "Error":
+            self.ids.calc_input.text = "Error"
+        else:
+            self.ids.calc_input.text = str(eval(prior))
+
+    def check_text(self, text):
+        divide = False
+        operator = ["*", "/", "-", "+"]
+        # this check is to ensure that we have pass in valid calculation
+        if text[-1] in operator:
+            text = text + text[:-1]
+            return text
+        # check for division by 0 and throw an error if found
+        for i in range(len(text)):
+            if text[i] == "/" and i < len(text):
+                if text[i + 1] == "0":
+                    return "Error"
+        
+        return text
+                     
+                
+
+class EstimateIncome(Screen):
+    pass
+
+
+
+    # # create equals to function
+    # def equals(self):
+    #     prior = self.ids.calc_input.text
+    #     print(prior)
+    #     # Addition
+    #     if "+" in prior:
+    #         num_list = prior.split("+")
+    #         answer = 0.0
+    #         # loop thru our list
+    #         for number in num_list:
+    #             answer = answer + float(number)
+
+    #         # print the answer in the text box
+    #         self.ids.calc_input.text = str(answer)
+
+
+class CalculatorApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(MyLayout(name = 'menu'))
+        sm.add_widget(EstimateIncome(name = 'estimate'))
+        return sm
 
 if __name__ == '__main__':
-    app = MainApp()
-    app.run()
+    CalculatorApp().run()
